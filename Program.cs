2b8a61420903e00e83b045d43c106750ea5214fb
InvaderCipher <55230649+InvaderCipher@users.noreply.github.com>
2019-10-29 16:09:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Opdracht_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string naam;
+
+			Console.WriteLine("Geef uw naam: ");
+
+			naam = Console.ReadLine();
+
+			Console.WriteLine("Geef uw leeftijd: ");
+
+			string leeftijd = Console.ReadLine();
+
+			Console.WriteLine("Uw naam is " + naam);
+			Console.WriteLine("Uw leeftijd is " + leeftijd);
+
+			Console.ReadLine(); 
+		}
+	}
+}
